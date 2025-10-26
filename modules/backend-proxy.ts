e718585e500049238e4037a1e762986d7fc8c279
unclecode <unclecode@kidocode.com>
2025-10-26 18:53:06 +0800

    Add custom backend proxy to handle IP address properly

---
+++ modules/backend-proxy.ts
@@ -0,0 +1,23 @@
+import { ZuploContext, ZuploRequest } from "@zuplo/runtime";
+
+export default async function (request: ZuploRequest, context: ZuploContext) {
+  const backendUrl = "http://129.212.146.238";
+
+  // Construct the full URL
+  const url = new URL(request.url);
+  const targetUrl = `${backendUrl}${url.pathname}${url.search}`;
+
+  // Forward the request to the backend
+  const response = await fetch(targetUrl, {
+    method: request.method,
+    headers: {
+      "Content-Type": "application/json",
+      "X-API-Key": "test-key-12345" // Backend expects this
+    },
+    body: request.method !== "GET" && request.method !== "HEAD"
+      ? await request.text()
+      : undefined,
+  });
+
+  return response;
+}
